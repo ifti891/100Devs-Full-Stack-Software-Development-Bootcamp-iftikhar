2dd762643540914130aa0213e688061ba323fb47
Iftikhar <ifti891@gmail.com>
2022-02-21 23:32:31 +0530

    working on new JS project

---
+++ Anki/anki.md
@@ -0,0 +1,18 @@
+## Lecture 12
+
+### JavaScript
+
+[] for arrays
+
+{} curly braces
+
+<> not brackets instead less than and greater than
+
+? turnery selector
+
+% moddulas (get remainders)
+
+#, ackto therp
+
+! bang
+
--- Lecture 12/class-weekend-boring/js/main.js
+++ Lecture 12/class-weekend-boring/js/main.js
@@ -4,7 +4,24 @@ function check() {
 
   const day = document.querySelector('#day').value
 
-  //Conditionals go here
+    if(day === "Wednesday" || day === "Friday"){
+      document.write('CLASS DAY');
+    }else if(day === "Saturday" || day === "Sunday"){
+      document.write('Weekend');
+    }else{
+      document.write('Booring');
+    }
 
-
+    document.getElementById('placeToSee').innerText = value;
 }
+
+
+// if (day === "Wednesday" || day === "Friday"){
+//   alert("It A Class Day")
+// } 
+// else(day === "Saturday" || day === "Sunday"){
+//   alert("Weekend")
+// } 
+// else{
+//   alert("Boooooooring")
+// }

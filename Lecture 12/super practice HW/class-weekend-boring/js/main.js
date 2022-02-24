575786bc608507bf61e8ed5b6fd01106147150fb
Iftikhar <ifti891@gmail.com>
2022-02-25 00:22:32 +0530

    project update

--- Lecture 12/super practice HW/class-weekend-boring/js/main.js
+++ Lecture 12/super practice HW/class-weekend-boring/js/main.js
@@ -1,39 +1,61 @@
-// document.querySelector("#check").addEventListener('click', check)
+
+// document.querySelector('#check').addEventListener('click', check)
+
 
 // function check() {
-//   const day = document.querySelector("#day").value
-//   var output = ''
 
-//   if (day === "Wednesday" || day === "Friday") {
-//     output = "CLASS DAYYYYY";
-//   }else if(day === "Saturday" || day === "Sunday"){
-//     output = "Weekend";
-//   }else {
-//     output = "Boooooring";
+//   const day = document.querySelector('#day').value.toLowerCase()
+//   let output = ''
+
+//   if(day === "tuesday" || day === "thursday"){
+//     output = "YOU HAVE CLASS"
+//   }else if( day === "saturday" || day === "sunday"){
+//     output = "Its The Weekend"
+//   }else{
+//     output = "BORING"
 //   }
-//   document.getElementbyId("placeToSee").innerText = output;
+//   document.getElementById('placeToSee').innerText = output
 // }
 
 
 
-document.querySelector('#check').addEventListener('click', check)
 
+document.querySelector("#check").addEventListener("click", check)
 
 function check() {
+  const day = document.querySelector("#day").value.toLowerCase()
+  let output = ''
 
-  const day = document.querySelector('#day').value
-  var output = ''
-
-    if(day === "Wednesday" || day === "Friday"){
-      output = 'CLASS DAY';
-    }else if(day === "Saturday" || day === "Sunday"){
-      output = 'Weekend';
-    }else{
-      output = 'Booring';
-    }
-
-    document.getElementById('placeToSee').innerText = output;
+  if (day === "wednesday" || day === "friday") {
+    output = "CLASS DAYYYYY"
+  }else if( day === "saturday" || day === "sunday"){
+    output = "Weekend"
+  }else {
+    output = "Boooooring"
+  }
+  document.getElementbyId("placeToSee").innerText = output;
 }
+
+
+
+
+
+
+// function check() {
+
+//   const day = document.querySelector('#day').value.toLowerCase()
+//   var output
+
+//     if(day === "wednesday" || day === "friday"){
+//       output = 'CLASS DAY';
+//     }else if(day === "saturday" || day === "sunday"){
+//       output = 'Weekend';
+//     }else{
+//       output = 'Booring';
+//     }
+
+//     document.getElementById('placeToSee').innerText = output;
+// }
 
 
 

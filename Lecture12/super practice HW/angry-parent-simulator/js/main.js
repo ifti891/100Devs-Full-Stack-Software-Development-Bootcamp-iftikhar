af88b86088e8d97cd0f881e0ed07533b04e3b581
Iftikhar <ifti891@gmail.com>
2022-02-27 09:49:13 +0530

    improving proects

--- Lecture12/super practice HW/angry-parent-simulator/js/main.js
+++ Lecture12/super practice HW/angry-parent-simulator/js/main.js
@@ -1,33 +1,148 @@
 
-document.querySelector('#yell').addEventListener('click', run)
+const synth = window.speechSynthesis;
+document.querySelector("#yell").addEventListener("click", run)
 
-function run() {
-  const fName = document.querySelector('#firstName').value
-  const fMidName = document.querySelector('#firstMiddle').value
-  const lMidName = document.querySelector('#lastMiddle').value
-  const lName = document.querySelector('#lastName').value
+function run(){
+  const fName = document.querySelector("#firstName").value
+  const fMiddle = document.querySelector("#firstMiddle").value
+  const lMiddle = document.querySelector("#lastMiddle").value
+  const lName = document.querySelector("#lastName").value
 
-  document.querySelector('#placeToYell').innerText = fName + ' ' + fMidName + ' ' + ' ' + lMidName + ' ' + lName
+  const yellText = `${fName} ${fMiddle} ${lMiddle} ${lName}`
+  document.querySelector("#placeToYell").innerText = yellText
 
-  //Add what you should be doing - conditionals go here
+  let yellThis = new SpeechSynthesisUtterance(yellText);
 
-  // document.querySelector('#placeToYell').innerText = `${fName} ${fMidName} ${lMidName} ${lName}`
+  synth.speak(yellThis)
 }
+
+
+// const synth = window.speechSynthesis;
+
+// document.querySelector("#yell").addEventListener("click", run)
+
+// function run(){
+//   const fName = document.querySelector("#firstName").value
+//   const fMiddle = document.querySelector("#firstMiddle").value
+//   const lMiddle = document.querySelector("#lastMiddle").value
+//   const lName = document.querySelector("#lastName").value
+
+//   const yellText = `${fName} ${fMiddle} ${lMiddle} ${lName}`
+
+//   document.querySelector("#placeToYell").innerText = yellText
+
+//   let yellThis = new SpeechSynthesisUtterance(yellText);
+
+
+//   synth.speak(yellThis);
+
+// }
+
+
+
+// const synth = window.speechSynthesis;
+
+// document.querySelector("#yell").addEventListener("click", run)
+
+
+// function run(){
+//   const fName = document.querySelector("#firstName").value
+//   const fMiddle = document.querySelector("#firstMiddle").value
+//   const lMiddle = document.querySelector("#lastMiddle").value
+//   const lName = document.querySelector("#lastName").value
+
+//   const yellText = `${fName} ${fMiddle} ${lMiddle} ${lName}`
+
+//   document.querySelector("#placeToYell").innerText = yellText
+
+//   let yellThis = new SpeechSynthesisUtterance(yellText);
+
+  
+//   synth.speak(yellThis);
+
+// }
+
+
+
 
 // const synth = window.speechSynthesis;
 // document.querySelector('#yell').addEventListener('click', run)
-//
+
 // function run() {
 //   const fName = document.querySelector('#firstName').value
 //   const fMidName = document.querySelector('#firstMiddle').value
 //   const lMidName = document.querySelector('#lastMiddle').value
 //   const lName = document.querySelector('#lastName').value
-//
+
 //   const yellText =  `${fName} ${fMidName} ${lMidName} ${lName}`
-//
+
 //   document.querySelector('#placeToYell').innerText = yellText
-//
+
 //   let yellThis = new SpeechSynthesisUtterance(yellText);
-//
+
 //   synth.speak(yellThis);
 // }
+
+
+
+
+
+
+
+
+
+// document.querySelector("#yell").addEventListener("click", run)
+
+// function run(){
+//   const fName = document.querySelector("#firstName").value
+//   const fMiddle = document.querySelector("#firstMiddle").value
+//   const lMiddle = document.querySelector("#lastMiddle").value
+//   const lName = document.querySelector("#lastName").value
+
+//   document.querySelector("#placeToYell").innerText = `${fName} ${fMiddle} ${lMiddle} ${lName}`
+// }
+
+
+
+
+
+
+
+
+
+
+// document.querySelector("#yell").addEventListener("click", run)
+
+
+// function run(){
+  // const fName = document.querySelector("#firstName").value
+//   const fMName = document.querySelector("#firstMiddle").value
+//   const lMiddle = document.querySelector("#lastMiddle").value
+//   const lName = document.querySelector("#lastName").value
+
+//   document.querySelector("#placeToYell").innerText = `${fName} ${fMName} ${lMiddle} ${lName}`
+// }
+
+
+
+// document.querySelector('#yell').addEventListener('click', run)
+
+// function run() {
+//   const fName = document.querySelector('#firstName').value
+//   const fMidName = document.querySelector('#firstMiddle').value
+//   const lMidName = document.querySelector('#lastMiddle').value
+//   const lName = document.querySelector('#lastName').value
+
+//   // document.querySelector('#placeToYell').innerText = fName + ' ' + fMidName + ' ' + ' ' + lMidName + ' ' + lName
+
+//   // Add what you should be doing - conditionals go here
+
+//   document.querySelector('#placeToYell').innerText = `${fName} ${fMidName} ${lMidName} ${lName}`
+// }
+
+
+
+
+
+
+
--- Lecture12/super practice HW/simple-calculator/js/main.js
+++ Lecture12/super practice HW/simple-calculator/js/main.js
@@ -1,32 +1,170 @@
 let total = 0
 
-document.querySelector('#pumpkin').addEventListener('click', makeZero)
-document.querySelector('#dominosPizza').addEventListener('click', jumanji)
-document.querySelector('#zebra').addEventListener('click', add9)
-document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
+document.querySelector("#pumpkin").addEventListener("click", makeZero)
+document.querySelector("#dominosPizza").addEventListener("click", add3)
+document.querySelector("#zebra").addEventListener("click", add9)
+document.querySelector("#cantThinkOfAnything").addEventListener("click", sub2)
 document.querySelector("#anotherNumber").addEventListener("click", add6)
 
-function makeZero() {
+function makeZero(){
   total = 0
-  document.querySelector('#placeToPutResult').innerText = total
+  document.querySelector("#placeToPutResult").innerText = total
 }
 
-function jumanji() {
+function add3(){
   total = total + 3
-  document.querySelector('#placeToPutResult').innerText = total
+  document.querySelector("#placeToPutResult").innerText = total
 }
 
-function add9() {
+function add9(){
   total = total + 9
-  document.querySelector('#placeToPutResult').innerHTML = total
+  document.querySelector("#placeToPutResult").innerText = total
 }
 
-function sub2() {
+function sub2(){
   total = total - 2
-  document.querySelector('#placeToPutResult').innerHTML = total
+  document.querySelector("#placeToPutResult").innerText = total
 }
 
 function add6(){
   total = total + 6
   document.querySelector("#placeToPutResult").innerText = total
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let total = 0
+
+// document.querySelector("#pumpkin").addEventListener("click", makeZero)
+// document.querySelector("#dominosPizza").addEventListener("click", add3)
+// document.querySelector("#zebra").addEventListener("click", add9)
+// document.querySelector("#cantThinkOfAnything").addEventListener("click", subtract2)
+// document.querySelector("#anotherNumber").addEventListener("click", add6)
+
+// function makeZero(){
+//   total = 0
+  
+//   document.querySelector("#placeToPutResult").innerText = total
+// }
+
+// function add3(){
+//   total = total + 3
+//   document.querySelector("#placeToPutResult").innerText = total
+  
+// }
+
+// function add9(){
+//   total = total + 9
+//   document.querySelector("#placeToPutResult").innerText = total
+// }
+
+// function subtract2(){
+//   total = total -2
+//   document.querySelector("#placeToPutResult").innerText = total
+// }
+
+// function add6(){
+//   total = total + 6
+//   document.querySelector("#placeToPutResult").innerText = total
+// }
+
+
+
+
+// let total = 0
+
+// document.querySelector("#pumpkin").addEventListener("click", makeZero)
+// document.querySelector("#dominosPizza").addEventListener("click", add3)
+// document.querySelector("#zebra").addEventListener("click", add9)
+// document.querySelector("#cantThinkOfAnything").addEventListener("click",subtract2)
+// document.querySelector("#anotherNumber").addEventListener("click", add6)
+
+// function makeZero(){
+//   total = 0
+//   document.querySelector("#placeToPutResult").innerText = total
+// }
+
+// function add3(){
+//   total = total + 3
+//   document.querySelector("#placeToPutResult").innerText = total
+// }
+
+// function add9(){
+//   total = total + 9
+//   document.querySelector("#placeToPutResult").innerText = total
+// }
+
+// function subtract2(){
+//   total = total -2
+//   document.querySelector("#placeToPutResult").innerText = total
+// }
+
+// function add6(){
+//   total = total + 6
+//   document.querySelector("#placeToPutResult").innerText = total
+// }
+
+
+
+
+// let total = 0
+
+// document.querySelector('#pumpkin').addEventListener('click', makeZero)
+// document.querySelector('#dominosPizza').addEventListener('click', jumanji)
+// document.querySelector('#zebra').addEventListener('click', add9)
+// document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
+// document.querySelector("#anotherNumber").addEventListener("click", add6)
+
+// function makeZero() {
+//   total = 0
+//   document.querySelector('#placeToPutResult').innerText = total
+// }
+
+// function jumanji() {
+//   total = total + 3
+//   document.querySelector('#placeToPutResult').innerText = total
+// }
+
+// function add9() {
+//   total = total + 9
+//   document.querySelector('#placeToPutResult').innerHTML = total
+// }
+
+// function sub2() {
+//   total = total - 2
+//   document.querySelector('#placeToPutResult').innerHTML = total
+// }
+
+// function add6(){
+//   total = total + 6
+//   document.querySelector("#placeToPutResult").innerText = total
+// }

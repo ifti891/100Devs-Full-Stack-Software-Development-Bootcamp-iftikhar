a95a084280b0c37d1f1a1800cd5174fbd941070b
Iftikhar <ifti891@gmail.com>
2022-03-01 19:16:31 +0530

    Added sounds to Youtube color picker

--- Lecture12/angry-parent-simulator/js/main.js
+++ Lecture12/angry-parent-simulator/js/main.js
@@ -1,18 +1,37 @@
 
-document.querySelector('#yell').addEventListener('click', run)
 
-function run() {
-  const fName = document.querySelector('#firstName').value
-  const fMidName = document.querySelector('#firstMiddle').value
-  const lMidName = document.querySelector('#lastMiddle').value
-  const lName = document.querySelector('#lastName').value
+// document.querySelector("#yell").addEventListener("click", run)
 
-  document.querySelector('#placeToYell').innerText = fName + ' ' + fMidName + ' ' + ' ' + lMidName + ' ' + lName
 
-  //Add what you should be doing - conditionals go here
+// function run(){
+//   const fName = document.querySelector("#firstName").value
+//   const fMiddle = document.querySelector("#firstMiddle").value
+//   const lMiddle = document.querySelector("#lastMiddle").value
+//   const lName = document.querySelector("#lastName").value
 
-  // document.querySelector('#placeToYell').innerText = `${fName} ${fMidName} ${lMidName} ${lName}`
-}
+//   document.querySelector("#placeToYell").innerText = `${fName} ${fMiddle} ${lMiddle} ${lName}`
+
+// }
+
+
+// document.querySelector('#yell').addEventListener('click', run)
+
+// function run() {
+//   const fName = document.querySelector('#firstName').value
+//   const fMidName = document.querySelector('#firstMiddle').value
+//   const lMidName = document.querySelector('#lastMiddle').value
+//   const lName = document.querySelector('#lastName').value
+
+//   // document.querySelector('#placeToYell').innerText = fName + ' ' + fMidName + ' ' + ' ' + lMidName + ' ' + lName
+
+//   //Add what you should be doing - conditionals go here
+
+//   document.querySelector('#placeToYell').innerText = `${fName} ${fMidName} ${lMidName} ${lName}`
+// }
+
+
+
+
 
 // const synth = window.speechSynthesis;
 // document.querySelector('#yell').addEventListener('click', run)
@@ -31,3 +50,23 @@ function run() {
 //
 //   synth.speak(yellThis);
 // }
+
+
+const bool = window.speechSynthesis;
+
+document.querySelector("#yell").addEventListener("click", run)
+
+function run(){
+  const fName = document.querySelector("#firstName").value
+  const fMiddle = document.querySelector("#firstMiddle").value
+  const lMiddle = document.querySelector("#lastMiddle").value
+  const lName = document.querySelector("#lastName").value
+
+  const yellText = `${fName} ${fMiddle} ${lMiddle} ${lName}`
+
+  document.querySelector("#placeToYell").innerText = yellText
+
+  let yellThis = new SpeechSynthesisUtterance(yellText)
+
+  bool.speak(yellThis)
+}
--- Lecture12/background-picker/js/main.js
+++ Lecture12/background-picker/js/main.js
@@ -1,3 +1,9 @@
+const colorPurple = window.speechSynthesis;
+const colorGreen = window.speechSynthesis;
+const colorBlue = window.speechSynthesis;
+const colorGold = window.speechSynthesis;
+
+
 document.getElementById('purple').onclick = partyPurple
 document.getElementById('green').onclick = partyGreen
 document.getElementById('blue').onclick = partyBlue
@@ -5,21 +11,36 @@ document.getElementById('gold').onclick = partyGold
 
 
 function partyPurple() {
-  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
-  document.querySelector('body').style.color = 'white'
+  const purple =  document.querySelector('body').style.backgroundColor = 'purple'
+  const white = document.querySelector('body').style.color = 'white'
+
+  let speakPurple = new SpeechSynthesisUtterance(purple) 
+
+  colorPurple.speak(speakPurple);
 }
 
 function partyGreen() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
-  document.querySelector('body').style.color = 'white'
+  const green = document.querySelector('body').style.backgroundColor = 'green'
+  const white =  document.querySelector('body').style.color = 'white'
+  let speakGreen = new SpeechSynthesisUtterance(green)
+
+  colorGreen.speak(speakGreen);
 }
 
 function partyBlue() {
-  document.querySelector('body').style.backgroundColor = 'rgb(101, 50, 221)'
-  document.querySelector('body').style.color = 'white'
+  const blue = document.querySelector('body').style.backgroundColor = 'blue'
+  const white = document.querySelector('body').style.color = 'white'
+
+  let speakBlue = new SpeechSynthesisUtterance(blue)
+
+  colorBlue.speak(speakBlue);
 }
 
 function partyGold(){
-  document.querySelector('body').style.backgroundColor = 'rgba(255, 215, 0)'
-  document.querySelector('body').style.color = 'white'
+  const gold = document.querySelector('body').style.backgroundColor = 'gold'
+  const white = document.querySelector('body').style.color = 'white'
+
+  let speakGold = new SpeechSynthesisUtterance(gold)
+
+  colorGold.speak(speakGold);
 }

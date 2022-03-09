8071d4d6ef679fc9b17b450ec298c982969f2b8e
Iftikhar <ifti891@gmail.com>
2022-03-09 22:21:31 +0530

    added solution

--- Leons solutions/class16-solutions/age-checker/js/main.js
+++ Leons solutions/class16-solutions/age-checker/js/main.js
@@ -1,34 +1,24 @@
 //Create a conditonal that checks their age
-
-let age = 31
 //If under 16, tell them they can not drive
-
-// let age = 16
-
-function age(){
-  if (age < 16){
-    console.log("they can not drive")
-  }else if (age < 18){
-    console.log("tell them they can't hate from outside the club, because they can't even get in")
-  }else if (age  < 21){
-    console.log("they can not drink")
-  }else if(age < 25){
-    console.log(" they can not rent cars")
-  }else if(age <= 30){
-    console.log("they can not rent fancy cars affordably")
-  }else if(age > 30){
-    console.log("there is nothing left to look forward too")
-  }
-
-  
-}
-
-
 //If under 18, tell them they can't hate from outside the club, because they can't even get in
 //If under 21, tell them they can not drink
 //If under 25, tell them they can not rent cars affordably
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
+let age = 30
+if(age < 16){
+    console.log('they can not drive')
+}else if(age < 18){
+    console.log('they can\'t hate from outside the club, because they can\'t even get in')
+}else if(age < 21){
+    console.log('they can not drink')
+}else if(age < 25){
+    console.log('they can not rent cars affordably')
+}else if(age < 30 ){
+    console.log('they can not rent fancy cars affordably')
+}else if(age >= 30){
+    console.log('there is nothing left to look forward too which is a lie - 100devs')
+}
 
 
 //--- Harder

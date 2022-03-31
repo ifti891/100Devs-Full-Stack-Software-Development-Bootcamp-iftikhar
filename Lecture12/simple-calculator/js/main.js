0ab6784ff2dfe11b6cd3b5437de789b948091e51
Iftikhar <iftikhar.sy@gmail.com>
2022-04-01 00:51:33 +0530

    Update main.js

--- Lecture12/simple-calculator/js/main.js
+++ Lecture12/simple-calculator/js/main.js
@@ -1,37 +1,37 @@
-// let count = 0;
+let count = 0;
 
-// document.querySelector("#pumpkin").addEventListener("click", makeZero)
-// document.querySelector("#dominosPizza").addEventListener("click", jumanji)
-// document.querySelector("#zebra").addEventListener("click", add9)
-// document.querySelector("#cantThinkOfAnything").addEventListener("click", sub2)
-// document.querySelector("#anotherNumber").addEventListener("click", add6
-
-
-    // function makeZero() {
-    //   total = 0;
-    //   document.querySelector("#placeToPutResult").innerText = total
-    // }
+document.querySelector("#pumpkin").addEventListener("click", makeZero)
+document.querySelector("#dominosPizza").addEventListener("click", jumanji)
+document.querySelector("#zebra").addEventListener("click", add9)
+document.querySelector("#cantThinkOfAnything").addEventListener("click", sub2)
+document.querySelector("#anotherNumber").addEventListener("click", add6
 
 
-    // function add3(){
-    //   total += 3;
-    //   document.querySelector("#placeToPutResult").innerText = total
-    // }
+    function makeZero() {
+      total = 0;
+      document.querySelector("#placeToPutResult").innerText = total
+    }
 
-    // function add9(){
-    //   totat += 9;
-    //   document.querySelctor("#placeToPutResult").innerText = total
-    // }
 
-    // function sub2(){
-    //   total -=2;
-    //   document.querySelector("#placeToPutResult").innerText = total
-    // }
+    function add3(){
+      total += 3;
+      document.querySelector("#placeToPutResult").innerText = total
+    }
 
-    // function add6(){
-    //   total += 6;
-    //   document.querySelector("#placeToPutResult").innertext = total
-    // }
+    function add9(){
+      totat += 9;
+      document.querySelctor("#placeToPutResult").innerText = total
+    }
+
+    function sub2(){
+      total -=2;
+      document.querySelector("#placeToPutResult").innerText = total
+    }
+
+    function add6(){
+      total += 6;
+      document.querySelector("#placeToPutResult").innertext = total
+    }
 
 
 

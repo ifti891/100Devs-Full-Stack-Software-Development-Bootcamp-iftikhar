7fd848f0d0464801f6deeb0b2f10b576e4de5629
Iftikhar <iftikhar.sy@gmail.com>
2022-04-01 00:42:18 +0530

    practicing

--- Lecture12/background-picker/js/main.js
+++ Lecture12/background-picker/js/main.js
@@ -1,8 +1,8 @@
 
-  const colorPurple = window.speechSynthesis;
-  const colorGreen = window.speechSynthesis;
-  const colorBlue = window.speechSynthesis;
-  const colorGold = window.speechSynthesis;
+  // const colorPurple = window.speechSynthesis;
+  // const colorGreen = window.speechSynthesis;
+  // const colorBlue = window.speechSynthesis;
+  // const colorGold = window.speechSynthesis;
 
 
 document.getElementById("purple").onclick = partyPurple
@@ -11,47 +11,89 @@ document.getElementById("blue").onclick = partyBlue
 document.getElementById("gold").onclick = partyGold
 
 
+const colorPurple = window.speechSynthesis;
+const colorGreen = window.speechSynthesis;
+const colorBlue = window.speechSynthesis;
+const colorGOld = window.speechSynthesis;
 
 
-
-function partyPurple(){
+function partyPurple (){
   const purple = document.querySelector("body").style.backgroundColor = "purple"
   const white = document.querySelector("body").style.color = "white"
 
-  let speakPurple = new SpeechSynthesisUtterance(purple)
+  let speakPurple = new speechSynthesisUtterance(purple)
 
-  colorPurple.speak(speakPurple);
+  colorPurple.speak(speakPurple)
 }
-
 
 function partyGreen(){
   const green = document.querySelector("body").style.backgroundColor = "green"
   const white = document.querySelector("body").style.color = "white"
 
-  let speakGreen = new SpeechSynthesisUtterance(green)
+  let speakGreen = new speechSynthesisUtterance(green)
 
-  colorGreen.speak(speakGreen);
-
+  colorGreen.speak(speakGreen)
 }
 
 function partyBlue(){
-  const blue = document.querySelector("body").style.backgroundColor = "blue"
+  const blue = document.querySlector("body").style.backgroundColor = "blue"
   const white = document.querySelector("body").style.color = "white"
 
-  let speakBlue = new SpeechSynthesisUtterance(blue)
+  let speakBlue = new speechSynthesisUtterance(blue)
 
-  colorBlue.speak(speakBlue);
+  colorBlue.speak(speakBlue)
 }
 
 function partyGold(){
-  const gold = document.querySelector("body").style.background = "gold"
+  const gold = document.querySelector("body").style.backgroundColor = "gold"
+  const  white = document.querySelector("body").style.color = "white"
 
-  const white = document.querySelector("body").style.color = "white"
-
-  let speakGold = new SpeechSynthesisUtterance(gold)
+  let speakGold = new speechSynthesisUtterance(gold)
 
   colorGold.speak(speakGold)
 }
+
+
+
+
+// function partyPurple(){
+//   const purple = document.querySelector("body").style.backgroundColor = "purple"
+//   const white = document.querySelector("body").style.color = "white"
+
+//   let speakPurple = new SpeechSynthesisUtterance(purple)
+
+//   colorPurple.speak(speakPurple);
+// }
+
+
+// function partyGreen(){
+//   const green = document.querySelector("body").style.backgroundColor = "green"
+//   const white = document.querySelector("body").style.color = "white"
+
+//   let speakGreen = new SpeechSynthesisUtterance(green)
+
+//   colorGreen.speak(speakGreen);
+
+// }
+
+// function partyBlue(){
+//   const blue = document.querySelector("body").style.backgroundColor = "blue"
+//   const white = document.querySelector("body").style.color = "white"
+
+//   let speakBlue = new SpeechSynthesisUtterance(blue)
+
+//   colorBlue.speak(speakBlue);
+// }
+
+// function partyGold(){
+//   const gold = document.querySelector("body").style.background = "gold"
+
+//   const white = document.querySelector("body").style.color = "white"
+
+//   let speakGold = new SpeechSynthesisUtterance(gold)
+
+//   colorGold.speak(speakGold)
+// }
 
 
 

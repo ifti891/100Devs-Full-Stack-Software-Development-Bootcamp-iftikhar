f5678a8d3d7a5be69347a1b12f14ae66268d1b47
Iftikhar <iftikhar.sy@gmail.com>
2022-04-03 11:58:32 +0530

    homeworking

--- Lecture13/simple-functions/js/main.js
+++ Lecture13/simple-functions/js/main.js
@@ -1,5 +1,21 @@
 //create a function that adds two numbers and alerts the sum
 
+function add2(a, b){
+    let sum = a + b;
+    alert(sum);
+}
+
+add2(1, 5);
+
+
 //create a function that multiplys three numbers and console logs the product
+
+function multThree(a, b, c){
+  let prod = (a * b * c);
+  return prod
+}
+
+console.log(multThree(2, 2, 2));
+
 
 //create a function that divides two numbers and returns the ???

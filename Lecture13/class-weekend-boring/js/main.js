14ccb2b4407b1b24e1684a3e3e470f80b297cfad
Iftikhar <iftikhar.sy@gmail.com>
2022-04-03 11:46:24 +0530

    Update main.js

--- Lecture13/class-weekend-boring/js/main.js
+++ Lecture13/class-weekend-boring/js/main.js
@@ -1,19 +1,37 @@
 //Handle Capital Letters, Place result in DOM, add a check for humpday (Wed)
 
-document.querySelector('#check').addEventListener('click', check)
+// document.querySelector('#check').addEventListener('click', check)
 
-function check() {
+// function check() {
 
-  const day = document.querySelector('#day').value.toLowerCase()
+//   const day = document.querySelector('#day').value.toLowerCase()
+//   let output = " ";
+//   if(day === "wednesday" || day === "friday"){
+//     output = "CLASS DAY"
+//   }else if( day === "saturday" || day === "sunday"){
+//     output = "WEEKEND";
+//   }else{
+//     output = "BOORING";
+//   }
+
+//   document.querySelector("#placeToSee").innerText = output
+
+// }
+
+
+document.querySelector("#check").addEventListener("click", run)
+function run(){
+  const day = document.querySelector("#day").value.toLowerCase()
+
   let output = " ";
+
   if(day === "wednesday" || day === "friday"){
-    output = "CLASS DAY"
-  }else if( day === "saturday" || day === "sunday"){
+    output = "CLASS DAY";
+  }else if(day === "saturday" || day === "sunday"){
     output = "WEEKEND";
   }else{
-    output = "BOORING";
+    output = "BOOORINNG";
   }
 
   document.querySelector("#placeToSee").innerText = output
-
 }

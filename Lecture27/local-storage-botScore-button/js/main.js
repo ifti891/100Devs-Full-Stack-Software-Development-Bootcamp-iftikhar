d6c74eb667e43ebce9ff71b1322559942c5fed59
iftikhar <iftikhar.sy@gmail.com>
2022-04-18 21:27:39 +0530

    game building

--- Lecture27/local-storage-botScore-button/js/main.js
+++ Lecture27/local-storage-botScore-button/js/main.js
@@ -1,3 +1,30 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
 
 
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+
+  console.log(choice);
+  
+  const url = `https://api.nasa.gov/planetary/apod?api_key=zhayDqkhe4LDCytLL2WsutKSI5ku9XZSk1mRMBzC&date=${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+
+        if(data.media_type === 'image'){
+          document.querySelector("img").src = data.hdurl
+        }else if(data.media_type === 'video'){
+          document.querySelector("iframe").src = data.url
+        }
+
+        
+        document.querySelector("h3").innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}

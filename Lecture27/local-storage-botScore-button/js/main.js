0415318a6ded0bf8b03f903ad9013f0c4dfd1466
iftikhar <iftikhar.sy@gmail.com>
2022-04-18 21:40:14 +0530

    card game

--- Lecture27/local-storage-botScore-button/js/main.js
+++ Lecture27/local-storage-botScore-button/js/main.js
@@ -1,30 +1,16 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
 
+if(!localStorage.getItem("botScore")){
+    localStorage.setItem("botScore", 0)
+    
+}
 
-document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
-  const choice = document.querySelector('input').value
 
-  console.log(choice);
-  
-  const url = `https://api.nasa.gov/planetary/apod?api_key=zhayDqkhe4LDCytLL2WsutKSI5ku9XZSk1mRMBzC&date=${choice}`
+document.querySelector('button').addEventListener('click', addAnothaOne)
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-
-        if(data.media_type === 'image'){
-          document.querySelector("img").src = data.hdurl
-        }else if(data.media_type === 'video'){
-          document.querySelector("iframe").src = data.url
-        }
-
-        
-        document.querySelector("h3").innerText = data.explanation
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+function addAnothaOne(){
+  let botScoreVal = Number(localStorage.getItem("botScore"))
+  botScoreVal += 1
+  localStorage.setItem("botScore", botScoreVal)
 }

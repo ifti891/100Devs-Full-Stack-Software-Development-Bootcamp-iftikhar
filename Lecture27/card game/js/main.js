4f619d71f14bef4120d2261d1a4fa2b088fbed0e
iftikhar <iftikhar.sy@gmail.com>
2022-04-21 11:21:17 +0530

    api fun

--- Lecture27/card game/js/main.js
+++ Lecture27/card game/js/main.js
@@ -1,33 +1,24 @@
 let deckId = " "
 
 
-fetch(https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1)
+fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        deckId = data.deck_id
 
-        if(data.media_type === 'image'){
-          document.querySelector("img").src = data.hdurl
-        }else if(data.media_type === 'video'){
-          document.querySelector("iframe").src = data.url
-        }
-
-        
-        document.querySelector("h3").innerText = data.explanation
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
-      
+
 
 document.querySelector('button').addEventListener('click', drawTwo)
 
 function drawTwo(){
-  const choice = document.querySelector('input').value
-
-  console.log(choice);
   
-  const url = `https://api.nasa.gov/planetary/apod?api_key=zhayDqkhe4LDCytLL2WsutKSI5ku9XZSk1mRMBzC&date=${choice}`
+  
+  const url = `https://deckofcardsapi.com/api/deck/w0ix97scd10p/draw/?count=2`
 
   
 }

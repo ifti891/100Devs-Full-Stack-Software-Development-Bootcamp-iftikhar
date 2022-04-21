fd2111f6616de0f135087ec9e20a9874b71fd402
iftikhar <iftikhar.sy@gmail.com>
2022-04-21 15:04:31 +0530

    card game api fun

--- Lecture27/card game/js/main.js
+++ Lecture27/card game/js/main.js
@@ -26,10 +26,34 @@ function drawTwo(){
         document.querySelector('#player1').src = data.cards[0].image 
         document.querySelector('#player2').src = data.cards[1].image
         
+        let player1Val = convertToNum(data.cards[0].value)
+        let player2Val = convertToNum(data.cards[1].value)
+
+        if(player1Val > player2Val){
+          document.querySelector("h3").innerText = "player 1 Wins"
+        } else if(player1Val < player2Val){
+          document.querySelector("h3").innerText = "player 2 Wins"
+        }else{
+          document.querySelector("h3").innerText = "Time for war"}
+
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+}
+
+function convertToNum(val){
+  if(val === "ACE"){
+    return 14
+  }else if(val === "KING"){
+    return 13
+  }else if (val === "QUEEN"){
+    return 12
+  }else if(val === "JACK"){
+    return 11
+  }else{
+    return Number(val)
+  }
 }
 
 

2ea2f230f35ee4549cbd069e9343bf49e6fc075b
Iftikhar #100Devs <ifti891@gmail.com>
2022-09-15 18:56:49 +0200

    added some code the app

--- Lecture35-super-review/objects-listen-to-your-heart/js/main.js
+++ Lecture35-super-review/objects-listen-to-your-heart/js/main.js
@@ -1,1 +1,20 @@
 //Create a stopwatch object that has four properties and three methods
+
+let stopwatch = {}
+
+stopwatch.color = 'Black'
+stopwatch.shape = 'Round'
+stopwatch.brand = 'Nike'
+stopwatch.condition = 'New'
+
+stopwatch.start = function(){
+  console.log("Runnnnnn")
+}
+
+stopwatch.stop = function(){
+  console.log("Stop runnnning")
+}
+
+stopwatch.time = function(){
+  console.log("look at you stopwatch")
+}

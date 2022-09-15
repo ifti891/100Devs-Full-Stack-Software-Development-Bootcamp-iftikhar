4ca050262e6265205288e331200943fe5e06adb5
Iftikhar #100Devs <ifti891@gmail.com>
2022-09-15 21:49:52 +0200

    added some code to the app

--- Lecture35-super-review/objects-dog-photos/js/main.js
+++ Lecture35-super-review/objects-dog-photos/js/main.js
@@ -1,1 +1,11 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+
+fetch('https://www.dog.ceo/api/breeds/image/random')
+.then(res => res.json()) // parse response as JSON_PROMISE
+.then(data => {
+  console.log(data.message)
+  document.querySelector('img').src = data.message
+})
+.catch(err => {
+  console.log(`error ${err}`)
+})
--- Lecture35-super-review/objects-street-fighter/js/main.js
+++ Lecture35-super-review/objects-street-fighter/js/main.js
@@ -1,1 +1,22 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function StreetFighter (name, health, speed, specialMove){
+
+  this.name = name
+  this.health = health
+  this.speed = speed
+  this.specialMove = specialMove
+}
+
+this.throw = function(){
+  console.log("Yeeeeeet")
+}
+
+this.taunt = function(){
+  console.log(`you cant handle my ${this.specialMove}`)
+}
+this.fatality = function(){
+  console.log("GETTTT OVERRRRRR HERRRRRRRRE")
+}
+
+let chinLi = new StreetFighter('Chun Li', 100, 100, 'Spining bird Kick')

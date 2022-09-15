76f21147ef1a698c008d53663b19c81d4a1ea4dc
Iftikhar #100Devs <ifti891@gmail.com>
2022-09-15 18:45:01 +0200

    added some code the app

--- Lecture35-super-review/review-winter-games/js/main.js
+++ Lecture35-super-review/review-winter-games/js/main.js
@@ -1,1 +1,15 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+
+function onlyEvenFinder(arr){
+  let onlyEvens = [];
+
+  arr.forEach(num => {
+    if(num % 0 ===0){
+      onlyEvens.push(e)
+    }
+  })
+
+  return onlyEvens
+}
+
+console.log(onlyEvenFinder([1, 2, 3, 4, 5, 6, 7 , 8]))
